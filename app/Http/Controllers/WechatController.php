95eabbc06330376bfe47fe99d7d6d69c139e2b69
whouu <whouu@qq.com>
2016-10-28 14:40:07 +0800

    增加第三方wechat sdk包

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Log;
+use EasyWeChat\Foundation\Application;
+
+/**
+ * Class WechatController
+ * @package App\Http\Controllers
+ */
+class WechatController extends Controller
+{
+
+    /**
+     * 处理微信请求
+     *
+     * @param Application $wechat
+     * @return mixed
+     */
+    public function serve(Application $wechat)
+    {
+        Log::info('request arrived');
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function ($message) {
+            return "欢迎关注 overtrue！";
+        });
+
+        Log::info('return response');
+        return $wechat->server->serve();
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/wechat','WechatController@serve');
