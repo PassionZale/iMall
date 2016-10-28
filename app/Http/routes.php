a5cb43e86858d7497d83bcee86b29d552f0f3381
Zhang Lei <372470745@qq.com>
2016-10-28 14:40:07 +0800

    overtrue/wechat=>overtrue/laravel-wechat

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/wechat','WechatController@serve');
+
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
+    Route::get('/user', function () {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+
+        dd($user);
+    });
+});
--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Overtrue\LaravelWechat\ServiceProvider::class,
+
     ],
 
     /*
@@ -201,7 +203,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'EasyWeChat' => Overtrue\LaravelWechat\Facade::class,
     ],
 
 ];
