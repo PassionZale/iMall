838fb7cc4828ceb5ea1be9f56ed591bdf6b8d409
whouu <whouu@qq.com>
2016-10-30 16:13:24 +0800

    wechat table

---
+++ database/migrations/2016_10_30_154027_create_wechat_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWechatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wechat', function (Blueprint $table) {
+            $table->increments('id');
+            // 名称
+            $table->string('wechat_name');
+            // 微信号
+            $table->string('wechat_account');
+            // 原始ID
+            $table->string('wechat_original_account');
+            // App Id
+            $table->string('app_id');
+            // App Secret
+            $table->string('app_secret');
+            $table->string('encodingaeskey');
+            // 校验token
+            $table->string('token');
+            // 服务器地址(自动生成)
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('wechat');
+    }
+}
