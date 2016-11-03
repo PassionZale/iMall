457ccbdb3592aeb6b470df8df66a8b7fbd2d5ba6
whouu <whouu@qq.com>
2016-11-03 09:44:28 +0800

    wechat config

--- config/wechat.php
+++ config/wechat.php
@@ -65,13 +65,13 @@ return [
      */
      'enable_mock' => env('WECHAT_ENABLE_MOCK', false),
      'mock_user' => [
-         "openid" =>"odh7zsgI75iT8FRh0fGlSojc9PWM",
+         "openid" =>"oE9sjwYunl6rc1SHaJA-0dCTfFuM",
          // 以下字段为 scope 为 snsapi_userinfo 时需要
-         "nickname" => "overtrue",
+         "nickname" => "iMall",
          "sex" =>"1",
-         "province" =>"北京",
-         "city" =>"北京",
+         "province" =>"广东",
+         "city" =>"深圳",
          "country" =>"中国",
-         "headimgurl" => "http://wx.qlogo.cn/mmopen/C2rEUskXQiblFYMUl9O0G05Q6pKibg7V1WpHX6CIQaic824apriabJw4r6EWxziaSt5BATrlbx1GVzwW2qjUCqtYpDvIJLjKgP1ug/0",
+         "headimgurl" => "http://wx.qlogo.cn/mmopen/ZibwoBM9icldvXG8VqGkTBHTILZ938qRrUzxuFK84TQqVP8kc4EqChefXyMfPBrdXtLjMnoXeg2qXAhsw1AxibGR5fVzIGCJPEf/0",
      ],
 ];
