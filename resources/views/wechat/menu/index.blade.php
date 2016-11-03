326ebefa3d2e7810b2acbf9c5bc0cac2a6ca18d7
whouu <whouu@qq.com>
2016-11-03 18:11:11 +0800

    发送菜单

--- resources/views/wechat/menu/index.blade.php
+++ resources/views/wechat/menu/index.blade.php
@@ -16,13 +16,13 @@
                 {{csrf_field()}}
             </form>
             @if(!empty($menus))
-                <table class="table table-responsive">
+                <table class="table table-responsive table-hover">
                     <thead>
                     <tr>
                         <th class="text-left">菜单名称</th>
                         <th>类型</th>
                         <th>关联关键词</th>
-                        <th>关联链接</th>
+                        <th class="text-left limit-width">关联链接</th>
                         <th>排序</th>
                         <th>操作</th>
                     </tr>
@@ -41,7 +41,7 @@
                                 @endif
                             </td>
                             <td>{{$menu->key}}</td>
-                            <td>{{$menu->url}}</td>
+                            <td title="{{$menu->url}}" class="limit-width text-left fn-text-overflow">{{$menu->url}}</td>
                             <td>{{$menu->sort}}</td>
                             <td>
                                 <a href="{{url('admin/wechat/menu/'.$menu->id.'/edit')}}" class="edit-btn">修改</a>
