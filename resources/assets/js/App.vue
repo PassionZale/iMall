2ef531a9fc8acd0e8333228fc5aab76fef368448
whouu <whouu@qq.com>
2016-11-11 18:16:10 +0800

    use vux

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -3,15 +3,38 @@
 </style>
 
 <template>
-    <d-n-d></d-n-d>
+    <!--<d-n-d></d-n-d>-->
     <router-view></router-view>
+    <div>
+        <tabbar>
+            <tabbar-item>
+                <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_button.png">
+                <span slot="label">Wechat</span>
+            </tabbar-item>
+            <tabbar-item show-dot>
+                <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_msg.png">
+                <span slot="label">Message</span>
+            </tabbar-item>
+            <tabbar-item selected>
+                <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_article.png">
+                <span slot="label">Explore</span>
+            </tabbar-item>
+            <tabbar-item>
+                <img slot="icon" src="https://o84lhz5xo.qnssl.com/master/src/assets/demo/icon_nav_cell.png">
+                <span slot="label">News</span>
+            </tabbar-item>
+        </tabbar>
+    </div>
 </template>
 
 <script>
     import DND from './components/common/DND.vue'
+    import Tabbar from 'vux/dist/components/tabbar'
+    import TabbarItem from 'vux/dist/components/tabbar-item'
+
     export default{
         components:{
-            DND
+            DND,Tabbar,TabbarItem
         },
         replace: false,
         data(){
--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -1,20 +1,30 @@
 <template>
-    <div>
-        <group>
-            <cell title="vue" value="cool"></cell>
-        </group>
-    </div>
+    <swiper :list="list" auto></swiper>
 </template>
 
-
 <script>
-import Group from 'vux/dist/components/group'
-import Cell from 'vux/dist/components/cell'
+import Swiper from 'vux/dist/components/swiper'
 
 export default {
   components: {
-    Group,
-    Cell
+    Swiper
+  },
+  data: function () {
+    return {
+      list: [{
+        url: '',
+        img: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjxnPjx0ZXh0IHg9IjMxNy43NDIxODc1IiB5PSIyNTAiIHN0eWxlPSJmaWxsOiM1NTU7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NDJwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj5GaXJzdCBzbGlkZTwvdGV4dD48L2c+PC9zdmc+',
+        title: '如何手制一份秋意的茶？'
+      }, {
+        url: '',
+        img: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiIvPjxnPjx0ZXh0IHg9IjI3Ny4yODkwNjI1IiB5PSIyNTAiIHN0eWxlPSJmaWxsOiM0NDQ7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NDJwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj5TZWNvbmQgc2xpZGU8L3RleHQ+PC9nPjwvc3ZnPg==',
+        title: '茶包VS原叶茶'
+      }, {
+        url: '',
+        img: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzU1NSIvPjxnPjx0ZXh0IHg9IjMwOC40Mjk2ODc1IiB5PSIyNTAiIHN0eWxlPSJmaWxsOiMzMzM7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NDJwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj5UaGlyZCBzbGlkZTwvdGV4dD48L2c+PC9zdmc+',
+        title: '播下茶籽，明春可发芽？'
+      }]
+    }
   }
 }
 </script>
