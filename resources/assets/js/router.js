85b6d88c70fe04c50fd263661293f12fa33a5b44
whouu <whouu@qq.com>
2016-11-16 14:43:30 +0800

    debug 轮播图在微信中不显示

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -6,7 +6,7 @@ export default function (router) {
     router.map({
         '/index': {
             name: 'index',
-            component:require('./components/Index/Banner.vue')
+            component:require('./components/Index.vue')
         },
         '/category':{
             name:'category',
