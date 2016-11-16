e7e911a715c34552b5880b545d27c2edc6dda2f7
whouu <whouu@qq.com>
2016-11-16 14:29:10 +0800

    debug 轮播图在微信中不显示

---
+++ resources/assets/js/components/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div>Index.vue</div>
+    </div>
+</template>
+
+<script>
+
+</script>
--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -6,7 +6,7 @@ export default function (router) {
     router.map({
         '/index': {
             name: 'index',
-            component:require('./components/Swiper.vue')
+            component:require('./components/Index.vue')
         },
         '/category':{
             name:'category',
