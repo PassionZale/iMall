8f63d45d98d6c60e710678a374af46be0220ea37
whouu <whouu@qq.com>
2016-11-18 12:33:57 +0800

    1

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -18,9 +18,19 @@
             }
         },
         created(){
+			this.isWeixin();
             this.fetchUser();
         },
         methods:{
+			isWeixin:function(){
+				let ua = window.navigator.userAgent.toLowerCase();
+				if(ua.match(/MicroMessenger/i) == 'micromessenger') {
+					// 若response.data为String类型，将其格式化成JSON类型
+					alert('微信内置浏览器');
+				}else{
+					alert('非微信浏览器');
+				}
+			},
             fetchUser:function(){
                 let userInfo = {};
                 userInfo = localStorage.getItem(userInfo);
