cb92612cee57a70cd531571f1c0dfd3ecb9e3dfd
whouu <whouu@qq.com>
2016-11-24 18:05:13 +0800

    购物车Vue Component

--- resources/assets/js/components/Cart.vue
+++ resources/assets/js/components/Cart.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-        <div>Cart.vue</div>
-    </div>
+    <section id="empty-cart-container" v-if="!cart.count">
+        <div class="empty-cart-wrapper">
+            <img src="/images/common/cart.png"/>
+            <h4>购物车快饿扁了T.T</h4>
+            <p>主人快给我挑点宝贝吧</p>
+            <a v-link="{name:'index'}">去逛逛</a>
+        </div>
+    </section>
 </template>
 
+<script>
+    export default{
+        data(){
+            return {
+                cart:{
+                    count:0,
+                    data:[]
+                }
+            }
+        },
+        ready(){
+            this.fetchCart();
+        },
+        methods:{
+            fetchCart:function(){
+                // TODO ajax load cart data
+                return false;
+            }
+        }
+    }
+
+</script>
