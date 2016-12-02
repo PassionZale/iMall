8449110c07e7bf607949bdeda9594848833f865e
whouu <whouu@qq.com>
2016-12-02 18:04:22 +0800

    UserCenter.vue Component

--- resources/assets/js/components/UserCenter.vue
+++ resources/assets/js/components/UserCenter.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
-        <div>UserCenter.vue</div>
-    </div>
+    <header id="uc-head-container">
+        <img class="bg-img" src="/images/common/uc-bg.jpg">
+        <img class="avatar-img" :src="user.avatar">
+    </header>
 </template>
+
+<script>
+    export default{
+        data(){
+            return {
+                user:''
+            }
+        },
+        ready(){
+            this.initHeader();
+        },
+        methods:{
+            initHeader:function(){
+                let vm = this;
+                vm.$set('user',vm.$parent.user);
+            }
+        }
+    }
+</script>
 
