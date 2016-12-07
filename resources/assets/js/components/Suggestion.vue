ee8d3163710c393d3f4396ba6bcb440e6d075913
whouu <whouu@qq.com>
2016-12-07 18:10:31 +0800

    意见建议组件

--- resources/assets/js/components/Suggestion.vue
+++ resources/assets/js/components/Suggestion.vue
@@ -1,19 +1,31 @@
 <template>
-    <div>
-        <div>this is template body</div>
-    </div>
+    <form @submit.prevent="submitSuggestion()" id="suggest-form">
+        <div class="form-wrapper">
+            <title>问题或建议</title>
+            <mt-field placeholder="请描述您的问题或建议，我们将尽快优化体验，感谢您的反馈。" type="textarea" rows="6"></mt-field>
+        </div>
+        <div class="form-wrapper">
+            <mt-button type="primary" size="large">提交</mt-button>
+        </div>
+    </form>
 </template>
 
 <script>
-
+    import { Field } from 'mint-ui';
+    import { Button } from 'mint-ui';
     export default{
         data(){
             return{
-                msg:'hello vue'
+                suggestion:''
             }
         },
         components:{
+            Field, Button
+        },
+        methods:{
+            submitSuggestion:function(){
 
+            }
         }
     }
 </script>
