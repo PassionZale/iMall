a8ab02f9f3af8b6174140e9f6f1f9d868374bc3b
whouu <whouu@qq.com>
2016-12-07 16:51:10 +0800

    用户中心

--- resources/assets/js/components/UserCenter.vue
+++ resources/assets/js/components/UserCenter.vue
@@ -3,14 +3,39 @@
         <img class="bg-img" src="/images/common/uc-bg.jpg">
         <img class="avatar-img" :src="user.avatar">
     </header>
+    <section id="uc-nav-container">
+        <div class="nav-wrapper">
+            <mt-cell title="待付款" is-link v-link="{name:'category'}">
+                <img slot="icon" src="/images/icon/order_unpay.png" width="24" height="24">
+            </mt-cell>
+            <mt-cell title="待发货" is-link v-link="{name:'category'}">
+                <img slot="icon" src="/images/icon/order_delivered.png" width="24" height="24">
+            </mt-cell>
+            <mt-cell title="待收货" is-link v-link="{name:'category'}">
+                <img slot="icon" src="/images/icon/order_receipt.png" width="24" height="24">
+            </mt-cell>
+            <mt-cell title="已完成" is-link v-link="{name:'category'}">
+                <img slot="icon" src="/images/icon/order_finish.png" width="24" height="24">
+            </mt-cell>
+        </div>
+        <div class="nav-wrapper">
+            <mt-cell title="意见建议" is-link v-link="{name:'category'}">
+                <img slot="icon" src="/images/icon/uc-suggestion.png" width="24" height="24">
+            </mt-cell>
+        </div>
+    </section>
 </template>
 
 <script>
+    import { Cell } from 'mint-ui'
     export default{
         data(){
             return {
                 user:''
             }
+        },
+        components:{
+            Cell
         },
         ready(){
             this.initHeader();
