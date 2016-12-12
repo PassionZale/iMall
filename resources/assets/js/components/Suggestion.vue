8ccec139b1b5a4152afb5033bc1f7fc37b624546
whouu <whouu@qq.com>
2016-12-12 17:48:23 +0800

    意见建议组件

--- resources/assets/js/components/Suggestion.vue
+++ resources/assets/js/components/Suggestion.vue
@@ -1,11 +1,17 @@
 <template>
-    <form @submit.prevent="submitSuggestion()" id="suggest-form">
+    <form id="suggest-form" @submit.prevent="submitSuggestion()">
         <div class="form-wrapper">
             <title>问题或建议</title>
-            <mt-field placeholder="请描述您的问题或建议，我们将尽快优化体验，感谢您的反馈。" type="textarea" rows="6"></mt-field>
+            <mt-field
+                    placeholder="请描述您的问题或建议，我们将尽快优化体验，感谢您的反馈。"
+                    slot="suggestion"
+                    type="textarea"
+                    rows="6"
+                    :value.sync="suggestion">
+            </mt-field>
         </div>
         <div class="form-wrapper">
-            <mt-button type="primary" size="large">提交</mt-button>
+            <mt-button :disabled="form_disabled" type="primary" size="large">提交</mt-button>
         </div>
     </form>
 </template>
@@ -16,15 +22,24 @@
     export default{
         data(){
             return{
-                suggestion:''
+                suggestion:'',
+                form_disabled:true
             }
         },
         components:{
             Field, Button
         },
+        watch:{
+            'suggestion':{
+                handler:function(val){
+                    val !== '' ? this.$set('form_disabled', false)
+                    : this.$set('form_disabled', true);
+                }
+            }
+        },
         methods:{
             submitSuggestion:function(){
-
+                console.log(this.suggestion);
             }
         }
     }
