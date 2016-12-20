47bdac48654e6a75faff07a554bb41373fd0af38
whouu <whouu@qq.com>
2016-12-20 18:14:22 +0800

    laravel预载入查出购物车及相关商品信息，定义一对一，购物车《=》商品，模型关系。

--- resources/assets/js/components/Cart.vue
+++ resources/assets/js/components/Cart.vue
@@ -1,10 +1,36 @@
 <template>
     <div id="cart-container">
-        <section v-for="cart in carts">
-            {{cart.commodity.commodity_name}}
+        <section class="cart-wrap"
+                 v-for="(cart, index) in carts">
+            <i class="select-one-btn"
+                @click="toggleSelect(index)">
+            </i>
+            <a class="img-wrap" v-link="{name:'commodity',params:{hashid:cart.commodity.id}}">
+                <img :src="cart.commodity.commodity_img"/>
+                <p class="name">{{cart.commodity.commodity_name}}</p>
+            </a>
+            <div class="price-wrap">
+                <span class="commodity-result">
+                    <span>&yen;{{cart.commodity.commodity_current_price}}</span>
+                    *
+                    <span class="num">{{cart.commodity_num}}</span>
+                    =
+                    <span class="price">&yen;{{cart.commodity.commodity_current_price * cart.commodity_num}}</span>
+                </span>
+                <div class="commodity-num">
+                        <span class="minus-btn"
+                              :class=" cart.commodity_num > 1 ? 'active' : '' "
+                              @click="minusClick(cart)">
+                        </span>
+                    <input class="num" type="tel" value="{{cart.commodity_num}}">
+                    <span class="plus-btn active"
+                          @click="plusClick(cart)">
+                    </span>
+                </div>
+            </div>
         </section>
     </div>
-    <div id="empty-cart-container" v-show="emptyVisible">
+    <div id="empty-cart-container" v-if="emptyVisible">
         <div class="empty-cart-wrapper">
             <img src="/images/common/cart.png"/>
             <h4>购物车快饿扁了T.T</h4>
@@ -21,6 +47,7 @@
         data(){
             return {
                 carts:'',
+                selected:[],
                 emptyVisible:false
             }
         },
@@ -40,6 +67,21 @@
                         vm.$set('emptyVisible',true);
                     }
                 });
+            },
+            minusClick: function(cart){
+                if(cart.commodity_num > 1){
+                    cart.commodity_num = cart.commodity_num - 1;
+                };
+            },
+            plusClick: function(cart){
+                cart.commodity_num = cart.commodity_num + 1;
+            },
+            toggleSelect: function(index){
+                if(this.selected.indexOf(index) == 0){
+                    this.selected.$remove(index);
+                }else{
+                    this.selected.push(index);
+                }
             }
         }
     }
