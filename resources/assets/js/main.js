f92f4ab6893b99fadde9b211a26d877389c6b82b
whouu <whouu@qq.com>
2016-12-30 11:36:39 +0800

    debug 微信端购物车点击去结算无法跳转至确认订单页面

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -43,8 +43,7 @@ axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('#csrf-to
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 const router = new Router({
-    history: true,
-    mode: 'html5'
+    history: true
 });
 
 routerMap(router);
