285a2dfaa426d575c5c98da3a017249b8d5af631
whouu <whouu@qq.com>
2017-01-03 18:03:57 +0800

    判断是否为本地环境，若是，则关闭JSSDK逻辑

--- app/Http/Controllers/Mall/IndexController.php
+++ app/Http/Controllers/Mall/IndexController.php
@@ -17,8 +17,13 @@ class IndexController extends Controller
 
     public function index()
     {
-        $wechat = app('wechat');
-        $js = $wechat->js;
+        $app_env = env('APP_ENV');
+        if($app_env != 'beta'){
+            $wechat = app('wechat');
+            $js = $wechat->js;
+        }else{
+            $js = [];
+        }
         return view('mall.index')->with(['js'=>$js]);
     }
 }
