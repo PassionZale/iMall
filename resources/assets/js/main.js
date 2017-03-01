9a6925740bf19a9352142908e790af77cbbf1421
whouu <whouu@qq.com>
2017-03-01 16:10:30 +0800

    修复微信端，前端请求还未结束切换路由时Indicator未关闭，仍继续运行的问题

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Mint from 'mint-ui'
-import {InfiniteScroll} from 'mint-ui';
+import {InfiniteScroll,Indicator} from 'mint-ui';
 import axios from 'axios'
 import Router from 'vue-router'
 import routerMap from './router'
@@ -65,6 +65,7 @@ routerMap(router);
 
 router.beforeEach((transition) => {
     document.body.scrollTop = 0;
+    Indicator.close();
     transition.next();
 });
 
