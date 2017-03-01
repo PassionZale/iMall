afb521a77577dbe684dac14271090bdb57472aa0
whouu <whouu@qq.com>
2017-03-01 14:37:03 +0800

    修复粉丝列表，点击更新按钮未正常刷新视图

--- app/Http/Controllers/Admin/WechatFollowController.php
+++ app/Http/Controllers/Admin/WechatFollowController.php
@@ -39,7 +39,7 @@ class WechatFollowController extends BaseController
             $data['remark'] = $user->remark;
             $data['groupid'] = $user->groupid;
             WechatFollow::where('openid', '=', $openid)->update($data);
-            return view('admin.wechat.follow.index')->withSuccess($follow['nickname'] . '信息更新成功！');
+            return redirect()->to('admin/wechat/follow/index')->withSuccess($follow['nickname'] . '信息更新成功！');
         }
     }
 
