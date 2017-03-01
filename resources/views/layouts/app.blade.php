e8367c78fcf333a77437b0e7c043ff64f6f4e013
whouu <whouu@qq.com>
2017-03-01 15:40:56 +0800

    订单模块增加视图部分

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,18 +64,11 @@
                     </a>
                 </li>
 
-                <li class="">
+                <li class="{{ request()->is('admin/order*') ? 'active' : '' }}">
                     <a href="{{url('admin/order')}}">
                         <i class="fa fa-align-justify"></i>
                         <span class="nav-label">订单管理</span>
                     </a>
-                    {{--<ul class="nav nav-second-level collapse">--}}
-                        {{--<li class=""><a href="#">待支付</a></li>--}}
-                        {{--<li class=""><a href="#">待发货</a></li>--}}
-                        {{--<li class=""><a href="#">已发货</a></li>--}}
-                        {{--<li class=""><a href="#">已完成</a></li>--}}
-                        {{--<li class=""><a href="#">已关闭</a></li>--}}
-                    {{--</ul>--}}
                 </li>
 
                 <li class="{{request()->is('admin/product/*') ? 'active' : ''}}">
